60889472df059fcad1c57bc61c9c31bfbb16df87
Elaheh Akbaripour <69744599+eliakbari@users.noreply.github.com>
2024-02-02 16:07:38 +0100

    Update myapp.sh

--- myapp.sh
+++ myapp.sh
@@ -6,66 +6,86 @@
 [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-$(uname)-arm64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
+
 export CTLPTL_VERSION="0.8.25"
 sudo curl -fsSL https://github.com/tilt-dev/ctlptl/releases/download/v$CTLPTL_VERSION/ctlptl.$CTLPTL_VERSION.linux.x86_64.tar.gz | sudo tar -xzv -C /usr/local/bin ctlptl
 sudo curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
 
-export CLUSTER_NAME="kind"
-
 # Check if the cluster already exists
-if kind get clusters | grep -q "^$CLUSTER_NAME$"; then
-    echo "Deleting existing cluster $CLUSTER_NAME"
-    kind delete cluster --name $CLUSTER_NAME
+if kind get clusters | grep -q "kind"; then
+    echo "Deleting existing cluster kind"
+    kind delete cluster --name kind
 fi
 
-# Step 1: Create a kube cluster using kind emulator with a local registry
-kind create cluster --name $CLUSTER_NAME --config - <<EOF
+
+set -o errexit
+
+# Create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
+    registry:2
+fi
+
+# Create kind cluster with containerd registry config dir enabled
+cat <<EOF | kind create cluster --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
   extraPortMappings:
-  - containerPort: 32000
-    hostPort: 32000
+  - containerPort: 30950
+    hostPort: 80
 containerdConfigPatches:
 - |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:32000"]
-    endpoint = ["http://localhost:32000"]
+  [plugins."io.containerd.grpc.v1.cri".registry]
+    config_path = "/etc/containerd/certs.d"
 EOF
 
-mkdir $HOME/certs
-sudo usermod -aG docker $USER
-sudo apt-get install openssl
+# Add the registry config to the nodes
+#
+# This is necessary because localhost resolves to loopback addresses that are
+# network-namespace local.
+# In other words: localhost in the container is not localhost on the host.
+#
+# We want a consistent name that works from both ends, so we tell containerd to
+# alias localhost:${reg_port} to the registry container when pulling images
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
+for node in $(kind get nodes); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+done
 
-IP_ADDRESS=$(hostname -I | awk '{print $1}') openssl req \
-    -new \
-    -newkey rsa:4096 \
-    -days 365 \
-    -nodes \
-    -x509 \
-    -subj "/C=GE/ST=BD/L=Stuttgart/O=MyOrg/CN=mytest.com" \
-    -reqexts req_ext \
-    -extensions req_ext \
-    -config openssl.cnf \
-    -keyout $HOME/certs/test.key \
-    -out $HOME/certs/test.cert
+# Connect the registry to the cluster network if not already connected
+# This allows kind to bootstrap the network but ensures they're on the same network
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
 
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 
-DOCKER_HOST_IP=$(hostname -I | awk '{print $1}')
-
-docker run -d -p 5000:5000 --restart=always --name registry \
--e REGISTRY_HTTP_TLS_CERTIFICATE=$HOME/certs/test.cert \
--e REGISTRY_HTTP_TLS_KEY=$HOME/certs/test.key \
--v $HOME/certs:$HOME/certs \
-registry:latest
-
-docker start registry
 docker build -t test:v1 .
-docker tag test:v1 $DOCKER_HOST_IP:5000/test:v1
-docker push $DOCKER_HOST_IP:5000/test:v1
-docker image rm test:v1
+docker tag test:v1 localhost:5001/test:v1
+docker push localhost:5001/test:v1
 
-# Step 1: Deployment should be a custom nginx container running on port 8081
+
+# Deployment should be a custom nginx container running on port 8081
 cat <<EOF > nginx-deployment.yaml
 apiVersion: apps/v1
 kind: Deployment
@@ -83,12 +103,12 @@ spec:
     spec:
       containers:
       - name: nginx
-        image: "${DOCKER_HOST_IP}:5000/test:v1"
+        image: localhost:5001/test:v1
         ports:
         - containerPort: 8081
 EOF
 
-# Step 2: Service should also be running on port 8081
+# Service should also be running on port 8081
 cat <<EOF > nginx-service.yaml
 apiVersion: v1
 kind: Service
@@ -102,15 +122,15 @@ spec:
     - protocol: TCP
       port: 8081
       targetPort: 8081
+      nodePort: 30950
 EOF
 
-# Step 3: Using tilt, deploy kube deployment/service to the kind cluster
+# Using tilt, deploy kube deployment/service to the kind cluster
 cat <<EOF > Tiltfile
 local('docker build -t my-custom-nginx .')
 k8s_yaml('nginx-deployment.yaml')
 k8s_yaml('nginx-service.yaml')
 EOF
-
 
 # Check the overall script status
 overall_status=$?
@@ -119,4 +139,3 @@ overall_status=$?
 echo "Overall Script Exit Status: $overall_status"
 
 tilt up
-
